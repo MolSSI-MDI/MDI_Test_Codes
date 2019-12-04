9c3f880cfd24f760b85a13d3587ce42f30192dde
Taylor Barnes <taylor.a.barnes@gmail.com>
2019-12-04 14:54:23 -0500

    Update .travis.yml

--- tests/test_mdi.py
+++ tests/test_mdi.py
@@ -1,27 +1,33 @@
 import os
 
+build_dir = "../build"
+
 ##########################
 # LIBRARY Method         #
 ##########################
 
 def test_cxx_cxx_lib():
-    cmd_return = os.system( 
-        "./$(find ../build/driver_lib_cxx*) -mdi \"-role DRIVER -name driver -method LIBRARY -out output\" " )
+    command = "cd " + build_dir + '''
+./$(find driver_lib_cxx*) -mdi \"-role DRIVER -name driver -method LIBRARY -out output\" '''
+
+    cmd_return = os.system( command )
     assert cmd_return == 0
 
     # read the output file
-    output_file = open("output", "r")
+    output_file = open(build_dir + "/output", "r")
     output = output_file.read()
 
     assert output == " Engine name: MM\n"
 
 def test_py_py_lib():
-    cmd_return = os.system( 
-        "python ../build/lib_py.py -mdi \"-role DRIVER -name driver -method LIBRARY -out output\" " )
+    command = "cd " + build_dir + '''
+python ../build/lib_py.py -mdi \"-role DRIVER -name driver -method LIBRARY -out output\" '''
+
+    cmd_return = os.system( command )
     assert cmd_return == 0
 
     # read the output file
-    output_file = open("output", "r")
+    output_file = open(build_dir + "/output", "r")
     output = output_file.read()
 
     expected = '''Start of driver
@@ -39,118 +45,127 @@ NATOMS: 123
 ##########################
 
 def test_cxx_cxx_mpi():
-    command = '''mpiexec -n 1 ./$(find ../build/driver_cxx*) -mdi \"-role DRIVER -name driver -method MPI -out output\" : \\
+    command = "cd " + build_dir + '''
+mpiexec -n 1 ./$(find ../build/driver_cxx*) -mdi \"-role DRIVER -name driver -method MPI -out output\" : \\
     -n 1 ./$(find ../build/engine_cxx*) -mdi \"-role ENGINE -name MM -method MPI\"'''
 
     cmd_return = os.system( command )
     #assert cmd_return == 0
 
     # read the output file
-    output_file = open("output", "r")
+    output_file = open(build_dir + "/output", "r")
     output = output_file.read()
 
     assert output == " Engine name: MM\n"
 
 def test_cxx_f90_mpi():
-    command = '''mpiexec -n 1 ./$(find ../build/driver_cxx*) -mdi \"-role DRIVER -name driver -method MPI -out output\" : \\
+    command = "cd " + build_dir + '''
+mpiexec -n 1 ./$(find ../build/driver_cxx*) -mdi \"-role DRIVER -name driver -method MPI -out output\" : \\
     -n 1 ./$(find ../build/engine_f90*) -mdi \"-role ENGINE -name MM -method MPI\"'''
 
     cmd_return = os.system( command )
     #assert cmd_return == 0
 
     # read the output file
-    output_file = open("output", "r")
+    output_file = open(build_dir + "/output", "r")
     output = output_file.read()
 
     assert output == " Engine name: MM\n"
 
 def test_cxx_py_mpi():
-    command = '''mpiexec -n 1 ./$(find ../build/driver_cxx*) -mdi \"-role DRIVER -name driver -method MPI -out output\" : \\
+    command = "cd " + build_dir + '''
+mpiexec -n 1 ./$(find ../build/driver_cxx*) -mdi \"-role DRIVER -name driver -method MPI -out output\" : \\
     -n 1 python ../build/engine_py.py -mdi \"-role ENGINE -name MM -method MPI\"'''
 
     cmd_return = os.system( command )
     #assert cmd_return == 0
 
     # read the output file
-    output_file = open("output", "r")
+    output_file = open(build_dir + "/output", "r")
     output = output_file.read()
 
     assert output == " Engine name: MM\n"
 
 def test_f90_cxx_mpi():
-    command = '''mpiexec -n 1 ./$(find ../build/driver_f90*) -mdi \"-role DRIVER -name driver -method MPI -out output\" : \\
+    command = "cd " + build_dir + '''
+mpiexec -n 1 ./$(find ../build/driver_f90*) -mdi \"-role DRIVER -name driver -method MPI -out output\" : \\
     -n 1 ./$(find ../build/engine_cxx*) -mdi \"-role ENGINE -name MM -method MPI\"'''
 
     cmd_return = os.system( command )
     #assert cmd_return == 0
 
     # read the output file
-    output_file = open("output", "r")
+    output_file = open(build_dir + "/output", "r")
     output = output_file.read()
 
     assert output == " Engine name: MM\n"
 
 def test_f90_f90_mpi():
-    command = '''mpiexec -n 1 ./$(find ../build/driver_f90*) -mdi \"-role DRIVER -name driver -method MPI -out output\" : \\
+    command = "cd " + build_dir + '''
+mpiexec -n 1 ./$(find ../build/driver_f90*) -mdi \"-role DRIVER -name driver -method MPI -out output\" : \\
     -n 1 ./$(find ../build/engine_f90*) -mdi \"-role ENGINE -name MM -method MPI\"'''
 
     cmd_return = os.system( command )
     #assert cmd_return == 0
 
     # read the output file
-    output_file = open("output", "r")
+    output_file = open(build_dir + "/output", "r")
     output = output_file.read()
 
     assert output == " Engine name: MM\n"
 
 def test_f90_py_mpi():
-    command = '''mpiexec -n 1 ./$(find ../build/driver_f90*) -mdi \"-role DRIVER -name driver -method MPI -out output\" : \\
+    command = "cd " + build_dir + '''
+mpiexec -n 1 ./$(find ../build/driver_f90*) -mdi \"-role DRIVER -name driver -method MPI -out output\" : \\
     -n 1 python ../build/engine_py.py -mdi \"-role ENGINE -name MM -method MPI\"'''
 
     cmd_return = os.system( command )
     #assert cmd_return == 0
 
     # read the output file
-    output_file = open("output", "r")
+    output_file = open(build_dir + "/output", "r")
     output = output_file.read()
 
     assert output == " Engine name: MM\n"
 
 def test_py_cxx_mpi():
-    command = '''mpiexec -n 1 python ../build/driver_py.py -mdi \"-role DRIVER -name driver -method MPI -out output\" : \\
+    command = "cd " + build_dir + '''
+mpiexec -n 1 python ../build/driver_py.py -mdi \"-role DRIVER -name driver -method MPI -out output\" : \\
     -n 1 ./$(find ../build/engine_cxx*) -mdi \"-role ENGINE -name MM -method MPI\"'''
 
     cmd_return = os.system( command )
     #assert cmd_return == 0
 
     # read the output file
-    output_file = open("output", "r")
+    output_file = open(build_dir + "/output", "r")
     output = output_file.read()
 
     assert output == " Engine name: MM\n"
 
 def test_py_f90_mpi():
-    command = '''mpiexec -n 1 python ../build/driver_py.py -mdi \"-role DRIVER -name driver -method MPI -out output\" : \\
+    command = "cd " + build_dir + '''
+mpiexec -n 1 python ../build/driver_py.py -mdi \"-role DRIVER -name driver -method MPI -out output\" : \\
     -n 1 ./$(find ../build/engine_f90*) -mdi \"-role ENGINE -name MM -method MPI\"'''
 
     cmd_return = os.system( command )
     #assert cmd_return == 0
 
     # read the output file
-    output_file = open("output", "r")
+    output_file = open(build_dir + "/output", "r")
     output = output_file.read()
 
     assert output == " Engine name: MM\n"
 
 def test_py_py_mpi():
-    command = '''mpiexec -n 1 python ../build/driver_py.py -mdi \"-role DRIVER -name driver -method MPI -out output\" : \\
+    command = "cd " + build_dir + '''
+mpiexec -n 1 python ../build/driver_py.py -mdi \"-role DRIVER -name driver -method MPI -out output\" : \\
     -n 1 python ../build/engine_py.py -mdi \"-role ENGINE -name MM -method MPI\"'''
 
     cmd_return = os.system( command )
     #assert cmd_return == 0
 
     # read the output file
-    output_file = open("output", "r")
+    output_file = open(build_dir + "/output", "r")
     output = output_file.read()
 
     assert output == " Engine name: MM\n"
@@ -162,127 +177,136 @@ def test_py_py_mpi():
 ##########################
 
 def test_cxx_cxx_tcp():
-    command = '''./$(find ../build/driver_cxx*) -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
-./$(find ../build/engine_cxx*) -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
+    command = "cd " + build_dir + '''
+./$(find driver_cxx*) -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
+./$(find engine_cxx*) -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
 wait'''
 
     cmd_return = os.system( command )
     assert cmd_return == 0
 
     # read the output file
-    output_file = open("output", "r")
+    output_file = open(build_dir + "/output", "r")
     output = output_file.read()
 
     assert output == " Engine name: MM\n"
 
 def test_cxx_f90_tcp():
-    command = '''./$(find ../build/driver_cxx*) -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
-./$(find ../build/engine_f90*) -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
+    command = "cd " + build_dir + '''
+./$(find driver_cxx*) -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
+./$(find engine_f90*) -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
 wait'''
 
     cmd_return = os.system( command )
     assert cmd_return == 0
 
     # read the output file
-    output_file = open("output", "r")
+    output_file = open(build_dir + "/output", "r")
     output = output_file.read()
 
     assert output == " Engine name: MM\n"
 
 def test_cxx_py_tcp():
-    command = '''./$(find ../build/driver_cxx*) -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
-python ../build/engine_py.py -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
+    command = "cd " + build_dir + '''
+./$(find driver_cxx*) -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
+python engine_py.py -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
 wait'''
 
     cmd_return = os.system( command )
     assert cmd_return == 0
 
     # read the output file
-    output_file = open("output", "r")
+    output_file = open(build_dir + "/output", "r")
     output = output_file.read()
 
     assert output == " Engine name: MM\n"
 
 def test_f90_cxx_tcp():
-    command = '''./$(find ../build/driver_f90*) -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
-./$(find ../build/engine_cxx*) -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
+    command = "cd " + build_dir + '''
+./$(find driver_f90*) -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
+./$(find engine_cxx*) -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
 wait'''
 
     cmd_return = os.system( command )
     assert cmd_return == 0
 
     # read the output file
-    output_file = open("output", "r")
+    output_file = open(build_dir + "/output", "r")
     output = output_file.read()
 
     assert output == " Engine name: MM\n"
 
 def test_f90_f90_tcp():
-    command = '''./$(find ../build/driver_f90*) -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
-./$(find ../build/engine_f90*) -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
+    command = "cd " + build_dir + '''
+./$(find driver_f90*) -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
+./$(find engine_f90*) -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
 wait'''
 
     cmd_return = os.system( command )
     assert cmd_return == 0
 
     # read the output file
-    output_file = open("output", "r")
+    output_file = open(build_dir + "/output", "r")
     output = output_file.read()
 
     assert output == " Engine name: MM\n"
 
 def test_f90_py_tcp():
-    command = '''./$(find ../build/driver_f90*) -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
-python ../build/engine_py.py -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
+    command = "cd " + build_dir + '''
+./$(find driver_f90*) -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
+python engine_py.py -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
 wait'''
 
     cmd_return = os.system( command )
     assert cmd_return == 0
 
     # read the output file
-    output_file = open("output", "r")
+    output_file = open(build_dir + "/output", "r")
     output = output_file.read()
 
     assert output == " Engine name: MM\n"
 
 def test_py_cxx_tcp():
-    command = '''python ../build/driver_py.py -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
-./$(find ../build/engine_cxx*) -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
+    command = "cd " + build_dir + '''
+python driver_py.py -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
+./$(find engine_cxx*) -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
 wait'''
 
     cmd_return = os.system( command )
     assert cmd_return == 0
 
     # read the output file
-    output_file = open("output", "r")
+    output_file = open(build_dir + "/output", "r")
     output = output_file.read()
 
     assert output == " Engine name: MM\n"
 
 def test_py_f90_tcp():
-    command = '''python ../build/driver_py.py -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
-./$(find ../build/engine_f90*) -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
+    command = "cd " + build_dir + '''
+python driver_py.py -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
+./$(find engine_f90*) -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
 wait'''
 
     cmd_return = os.system( command )
     assert cmd_return == 0
 
     # read the output file
-    output_file = open("output", "r")
+    output_file = open(build_dir + "/output", "r")
     output = output_file.read()
 
     assert output == " Engine name: MM\n"
 
 def test_py_py_tcp():
-    command = '''python ../build/driver_py.py -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
-python ../build/engine_py.py -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
+    command = "cd " + build_dir + '''
+python driver_py.py -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
+python engine_py.py -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
 wait'''
 
     cmd_return = os.system( command )
     assert cmd_return == 0
 
     # read the output file
-    output_file = open("output", "r")
+    output_file = open(build_dir + "/output", "r")
     output = output_file.read()
 
     assert output == " Engine name: MM\n"
