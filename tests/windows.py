73c496722bbc305ee43b7556607219995f778ddc
Taylor Barnes <taylor.a.barnes@gmail.com>
2019-12-04 16:47:48 -0500

    Update .travis.yml

--- tests/windows.py
+++ tests/windows.py
@@ -26,8 +26,8 @@ cmd_return = os.system( command )
 print("After command2")
 
 print("No redirect")
-command = '''./$(find ../build/driver_cxx*) -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
-./$(find ../build/engine_cxx*) -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
+command = '''./$(find driver_cxx*) -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
+./$(find engine_cxx*) -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
 wait'''
 cmd_return = os.system( command )
 print("After no redirect")
