a7661ae3c9d4c898953387c3678422a67633f384
Taylor Barnes <taylor.a.barnes@gmail.com>
2019-12-04 15:56:56 -0500

    Update windows.py

--- tests/windows.py
+++ tests/windows.py
@@ -16,3 +16,18 @@ wait'''
 print("Before command")
 cmd_return = os.system( command )
 print("After command")
+
+command = '''./$(find ../build/driver_cxx*) -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
+./$(find ../build/engine_cxx*) -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
+wait'''
+
+print("Before command2")
+cmd_return = os.system( command )
+print("After command2")
+
+print("No redirect")
+command = '''./$(find ../build/driver_cxx*) -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
+./$(find ../build/engine_cxx*) -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
+wait'''
+cmd_return = os.system( command )
+print("After no redirect")
