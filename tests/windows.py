2053617705164c32c959c3d070fb9f750d0cce1e
Taylor Barnes <taylor.a.barnes@gmail.com>
2019-12-04 15:50:46 -0500

    Update .travis.yml

--- tests/windows.py
+++ tests/windows.py
@@ -7,8 +7,12 @@ build_dir = "../build"
 ##########################
 
 command = "cd " + build_dir + '''
+mkdir AAAAAA_dir
+touch output
 ./$(find driver_cxx*) -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
 ./$(find engine_cxx*) -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
 wait'''
 
+print("Before command")
 cmd_return = os.system( command )
+print("After command")
