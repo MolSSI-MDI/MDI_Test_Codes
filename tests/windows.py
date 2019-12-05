20a53762aa71a6420137f2b4ffc5818c4497aa29
Taylor Barnes <taylor.a.barnes@gmail.com>
2019-12-05 09:13:23 -0500

    Update windows.py

--- tests/windows.py
+++ tests/windows.py
@@ -39,14 +39,14 @@ print("After no redirect")
 print("Error code: " + str(cmd_return))
 
 print("Mkdir")
-command = '''mkdir BBB1_dir
-mkdir BBB11_dir
-driver_cxx.exe -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &
-mkdir BBB2_dir
-engine_cxx.exe -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &
-mkdir BBB3_dir
-wait
-mkdir BBB4_dir'''
+command = "mkdir BBB1_dir;" + 
+"mkdir BBB11_dir;" + 
+"driver_cxx.exe -mdi \"-role DRIVER -name driver -method TCP -port 8021 -out output\" &;" +
+"mkdir BBB2_dir;" +
+"engine_cxx.exe -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\" &;" + 
+"mkdir BBB3_dir;" +
+"wait;" +
+"mkdir BBB4_dir"
 ierr = os.system(command)
 print("Error code: " + str(ierr))
 
