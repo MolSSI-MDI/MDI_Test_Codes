13566e316a3e48bcf57f293465476a3781676334
Taylor Barnes <taylor.a.barnes@gmail.com>
2019-12-05 11:53:49 -0500

    Update windows.py

--- tests/windows.py
+++ tests/windows.py
@@ -4,8 +4,8 @@ import subprocess
 print("Doing subprocess")
 
 # get the name of the driver
-#driver_name = subprocess.check_output(["find","../build/driver_cxx*"], shell=True)
-dproc = subprocess.Popen(["find ../build -maxdepth 1 -name driver_cxx*"], stdout=subprocess.PIPE, shell=True)
+#dproc = subprocess.Popen(["find ../build -maxdepth 1 -name driver_cxx*"], stdout=subprocess.PIPE, shell=True)
+dproc = subprocess.Popen(["find", "../build", "-maxdepth", "1", "-name", "driver_cxx*"], stdout=subprocess.PIPE)
 output, err = dproc.communicate()
 driver_name = output.decode('utf-8')
 driver_name = driver_name[:-1]
@@ -16,8 +16,8 @@ driver_name = driver_name[:-1]
 #engine_name = engine_name[:-1]
 
 #find ../build -maxdepth 1 -name "driver_cxx*"
-eproc = subprocess.Popen(["find ../build -maxdepth 1 -name engine_cxx*"], stdout=subprocess.PIPE, shell=True)
-#eproc = subprocess.Popen(['find','../build','-maxdepth','1','-name','engine_cxx*'], stdout=subprocess.PIPE, shell=True)
+#eproc = subprocess.Popen(["find ../build -maxdepth 1 -name engine_cxx*"], stdout=subprocess.PIPE, shell=True)
+eproc = subprocess.Popen(["find", "../build", "-maxdepth", "1", "-name", "engine_cxx*"], stdout=subprocess.PIPE)
 output, err = eproc.communicate()
 engine_name = output.decode('utf-8')
 engine_name = engine_name[:-1]
@@ -25,9 +25,12 @@ engine_name = engine_name[:-1]
 print("driver_name: " + str(driver_name))
 print("engine_name: " + str(engine_name))
 
-driver_proc = subprocess.Popen([driver_name + " -mdi \"-role DRIVER -name driver -method TCP -port 8021\""],
-                               stdout=subprocess.PIPE, shell=True)
-engine_proc = subprocess.Popen([engine_name + " -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\""], shell=True)
+#driver_proc = subprocess.Popen([driver_name + " -mdi \"-role DRIVER -name driver -method TCP -port 8021\""],
+#                               stdout=subprocess.PIPE, shell=True)
+driver_proc = subprocess.Popen([driver_name, "-mdi", "-role DRIVER -name driver -method TCP -port 8021"],
+                               stdout=subprocess.PIPE)
+#engine_proc = subprocess.Popen([engine_name + " -mdi \"-role ENGINE -name MM -method TCP -port 8021 -hostname localhost\""], shell=True)
+engine_proc = subprocess.Popen([engine_name, "-mdi", "-role ENGINE -name MM -method TCP -port 8021 -hostname localhost"])
 output, err = driver_proc.communicate()
 engine_proc.communicate()
 
